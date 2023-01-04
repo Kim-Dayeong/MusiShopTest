f2155ce86e4d2e2e39ec33a5960838a060ac219f
Kim-Dayeong <kkimdayeongg@gmail.com>
2023-01-04 22:24:10 +0900

    commit

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -55,135 +55,135 @@ public class ItemController {
 	}
 
 	//아이템 목록
-		@RequestMapping(value="/ItemListPage", method=RequestMethod.GET)
-		public void getListPage(Model model, @RequestParam("num")int num)throws Exception {
+//		@RequestMapping(value="/ItemListPage", method=RequestMethod.GET)
+//		public void getListPage(Model model, @RequestParam("num")int num)throws Exception {
 //			List<ItemVO> list = null;
 //			list = service.list();
 //			
 //			model.addAttribute("list",list);
 			
 			 // 게시물 총 갯수
-			 int count = service.count();
-			  
-			 // 한 페이지에 출력할 게시물 갯수
-			 int postNum = 10;
-			  
-			 // 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
-			 int pageNum = (int)Math.ceil((double)count/postNum);
-			  
-			 // 출력할 게시물
-			 int displayPost = (num - 1) * postNum;
-			 
-			 
-			// 한번에 표시할 페이징 번호의 갯수
-			 int pageNum_cnt = 10;
-
-			 // 표시되는 페이지 번호 중 마지막 번호
-			 int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_cnt) * pageNum_cnt);
-
-			 // 표시되는 페이지 번호 중 첫번째 번호
-			 int startPageNum = endPageNum - (pageNum_cnt - 1);
-			 
-			// 마지막 번호 재계산
-			 int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
-			  
-			 if(endPageNum > endPageNum_tmp) {
-			  endPageNum = endPageNum_tmp;
-			 }
-			 
-			 boolean prev = startPageNum == 1 ? false : true;
-			 boolean next = endPageNum * pageNum_cnt >= count ? false : true;
-			 
-			// 시작 및 끝 번호
-			 model.addAttribute("startPageNum", startPageNum);
-			 model.addAttribute("endPageNum", endPageNum);
-
-			 // 이전 및 다음 
-			 model.addAttribute("prev", prev);
-			 model.addAttribute("next", next);
-			 
-			 
-			// 현재 페이지
-			 model.addAttribute("select", num);
-			    
-			 List list = null; 
-			 list = service.listPage(displayPost, postNum);
-			 model.addAttribute("list", list);   
-			 model.addAttribute("pageNum", pageNum);
-			
-			 
-			 
-		}
-		
-		//아이템 페이지 검색 
-				@RequestMapping(value="/ItemPageSearch", method=RequestMethod.GET)
-				public void getListPageSearch(Model model, @RequestParam("num")int num)throws Exception {			
-					
-					 // 게시물 총 갯수
-					 int count = service.count();
-					  
-					 // 한 페이지에 출력할 게시물 갯수
-					 int postNum = 10;
-					  
-					 // 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
-					 int pageNum = (int)Math.ceil((double)count/postNum);
-					  
-					 // 출력할 게시물
-					 int displayPost = (num - 1) * postNum;
-					 
-					 
-					// 한번에 표시할 페이징 번호의 갯수
-					 int pageNum_cnt = 10;
-
-					 // 표시되는 페이지 번호 중 마지막 번호
-					 int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_cnt) * pageNum_cnt);
-
-					 // 표시되는 페이지 번호 중 첫번째 번호
-					 int startPageNum = endPageNum - (pageNum_cnt - 1);
-					 
-					// 마지막 번호 재계산
-					 int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
-					  
-					 if(endPageNum > endPageNum_tmp) {
-					  endPageNum = endPageNum_tmp;
-					 }
-					 
-					 boolean prev = startPageNum == 1 ? false : true;
-					 boolean next = endPageNum * pageNum_cnt >= count ? false : true;
-					 
-					// 시작 및 끝 번호
-					 model.addAttribute("startPageNum", startPageNum);
-					 model.addAttribute("endPageNum", endPageNum);
-
-					 // 이전 및 다음 
-					 model.addAttribute("prev", prev);
-					 model.addAttribute("next", next);
-					 
-					 
-					// 현재 페이지
-					 model.addAttribute("select", num);
-					    
-					 List list = null; 					 
-					 list = service.listPage(displayPost, postNum);
-					 
-					 model.addAttribute("list", list);   
-					 model.addAttribute("pageNum", pageNum);
-					 model.addAttribute("select", num);
-					
-					 
-					 
-				}
-				
-				// 아이템 목록 + 페이징 추가 + 검색
+//			 int count = service.count();
+//			  
+//			 // 한 페이지에 출력할 게시물 갯수
+//			 int postNum = 10;
+//			  
+//			 // 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
+//			 int pageNum = (int)Math.ceil((double)count/postNum);
+//			  
+//			 // 출력할 게시물
+//			 int displayPost = (num - 1) * postNum;
+//			 
+//			 
+//			// 한번에 표시할 페이징 번호의 갯수
+//			 int pageNum_cnt = 10;
+//
+//			 // 표시되는 페이지 번호 중 마지막 번호
+//			 int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_cnt) * pageNum_cnt);
+//
+//			 // 표시되는 페이지 번호 중 첫번째 번호
+//			 int startPageNum = endPageNum - (pageNum_cnt - 1);
+//			 
+//			// 마지막 번호 재계산
+//			 int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
+//			  
+//			 if(endPageNum > endPageNum_tmp) {
+//			  endPageNum = endPageNum_tmp;
+//			 }
+//			 
+//			 boolean prev = startPageNum == 1 ? false : true;
+//			 boolean next = endPageNum * pageNum_cnt >= count ? false : true;
+//			 
+//			// 시작 및 끝 번호
+//			 model.addAttribute("startPageNum", startPageNum);
+//			 model.addAttribute("endPageNum", endPageNum);
+//
+//			 // 이전 및 다음 
+//			 model.addAttribute("prev", prev);
+//			 model.addAttribute("next", next);
+//			 
+//			 
+//			// 현재 페이지
+//			 model.addAttribute("select", num);
+//			    
+//			 List list = null; 
+//			 list = service.listPage(displayPost, postNum);
+//			 model.addAttribute("list", list);   
+//			 model.addAttribute("pageNum", pageNum);
+//			
+//			 
+//			 
+//		}
+//		
+//		//아이템 페이지 검색 
+//				@RequestMapping(value="/ItemPageSearch", method=RequestMethod.GET)
+//				public void getListPageSearch(Model model, @RequestParam("num")int num)throws Exception {			
+//					
+//					 // 게시물 총 갯수
+//					 int count = service.count();
+//					  
+//					 // 한 페이지에 출력할 게시물 갯수
+//					 int postNum = 10;
+//					  
+//					 // 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
+//					 int pageNum = (int)Math.ceil((double)count/postNum);
+//					  
+//					 // 출력할 게시물
+//					 int displayPost = (num - 1) * postNum;
+//					 
+//					 
+//					// 한번에 표시할 페이징 번호의 갯수
+//					 int pageNum_cnt = 10;
+//
+//					 // 표시되는 페이지 번호 중 마지막 번호
+//					 int endPageNum = (int)(Math.ceil((double)num / (double)pageNum_cnt) * pageNum_cnt);
+//
+//					 // 표시되는 페이지 번호 중 첫번째 번호
+//					 int startPageNum = endPageNum - (pageNum_cnt - 1);
+//					 
+//					// 마지막 번호 재계산
+//					 int endPageNum_tmp = (int)(Math.ceil((double)count / (double)pageNum_cnt));
+//					  
+//					 if(endPageNum > endPageNum_tmp) {
+//					  endPageNum = endPageNum_tmp;
+//					 }
+//					 
+//					 boolean prev = startPageNum == 1 ? false : true;
+//					 boolean next = endPageNum * pageNum_cnt >= count ? false : true;
+//					 
+//					// 시작 및 끝 번호
+//					 model.addAttribute("startPageNum", startPageNum);
+//					 model.addAttribute("endPageNum", endPageNum);
+//
+//					 // 이전 및 다음 
+//					 model.addAttribute("prev", prev);
+//					 model.addAttribute("next", next);
+//					 
+//					 
+//					// 현재 페이지
+//					 model.addAttribute("select", num);
+//					    
+//					 List list = null; 					 
+//					 list = service.listPage(displayPost, postNum);
+//					 
+//					 model.addAttribute("list", list);   
+//					 model.addAttribute("pageNum", pageNum);
+//					 model.addAttribute("select", num);
+//					
+//					 
+//					 
+//				}
+//				
+//				// 아이템 목록 + 페이징 추가 + 검색
 				@RequestMapping(value = "/ItemPageSearch", method = RequestMethod.GET)
-				public void getListPageSearch(Model model, @RequestParam("num") int num, 
-				  @RequestParam("searchType") String searchType, @RequestParam("keyword") String keyword
-				  ) throws Exception {
+			public void getListPageSearch(Model model, @RequestParam("num") int num, 
+			  @RequestParam("searchType") String searchType, @RequestParam("keyword") String keyword
+			  ) throws Exception {
 					
-					 // 게시물 총 갯수
+				 // 게시물 총 갯수
 					 int count = service.count();
 					  
-					 // 한 페이지에 출력할 게시물 갯수
+				 // 한 페이지에 출력할 게시물 갯수
 					 int postNum = 10;
 					  
 					 // 하단 페이징 번호 ([ 게시물 총 갯수 ÷ 한 페이지에 출력할 갯수 ]의 올림)
@@ -212,14 +212,14 @@ public class ItemController {
 					 boolean prev = startPageNum == 1 ? false : true;
 					 boolean next = endPageNum * pageNum_cnt >= count ? false : true;
 					 
-					// 시작 및 끝 번호
+				// 시작 및 끝 번호
 					 model.addAttribute("startPageNum", startPageNum);
 					 model.addAttribute("endPageNum", endPageNum);
 
 					 // 이전 및 다음 
-					 model.addAttribute("prev", prev);
+				 model.addAttribute("prev", prev);
 					 model.addAttribute("next", next);
-					 
+				 
 					 
 					// 현재 페이지
 					 model.addAttribute("select", num);
@@ -227,18 +227,14 @@ public class ItemController {
 					 List list = null; 					 
 					 list = service.listPage(displayPost, postNum);
 					 
-					 list = service.listPageSearch(getDisplayPost(), getPostNum(), searchType, keyword);
+				 list = service.listPageSearch(getDisplayPost(), getPostNum(), searchType, keyword);
 					 
 					 model.addAttribute("list", list);   
 					 model.addAttribute("pageNum", pageNum);
 					 model.addAttribute("select", num);
-					
-
+					 
 				
-			
-
-				 
-				 model.addAttribute("list", list);
+					model.addAttribute("list", list);
 	
 				 model.addAttribute("select", num);
 				 
